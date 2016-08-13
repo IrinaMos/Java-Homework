33accf44694e3f9102b0e24a021a6250dbb0d00e
IrinaMos <irena.moskovich@gmail.com>
2016-08-13 20:43:20 +0300

    Revert "класс Point для представления точек на двумерной плоскости"
    This reverts commit 54a7e8bb8379e7bba8377fa9213447ae5fc0be73.

--- sandbox/src/main/java/il/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/il/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,28 +2,6 @@ package il.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-   //hello("world");
-    Point p1;
-    p1 = new Point();
-    Point p2 = new Point();
-    p1.x = 2;
-    p1.y = 5;
-    p2.x = 10;
-    p2.y = 8;
-    double res = distance (p1, p2);
-
-  //   System.out.println("rasstoyanie mezhdu dvumya tochkami " + " = " + res);
+    System.out.println("Hello, world!");
   }
-
-  public static void hello(String somebody) {
-    System.out.println("Hello, " + somebody +  "!");
-  }
-
-  public static double distance (Point p1, Point p2) {
-     double temp = ((p2.x - p1.x)*(p2.x - p1.x))+((p2.y - p1.y)*(p2.y - p1.y));
-    double res = Math.sqrt(temp);
-    System.out.println("the distance is " + res +  "!");
-    return (res);
-  }
-
 }
--- sandbox/src/main/java/il/stqa/pft/sandbox/Point.java
+++
@@ -1,9 +0,0 @@
-package il.stqa.pft.sandbox;
-
-/**
- * Created by Irena on 8/13/2016.
- */
-public class Point {
-  public double x;
-  public double y;
-}
