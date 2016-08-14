e3971aa4c1b14c8ec7fa004f5d8c1faa3d40c70a
IrinaMos <irena.moskovich@gmail.com>
2016-08-14 20:55:44 +0300

    Using method in the Point class

--- sandbox/src/main/java/il/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/il/stqa/pft/sandbox/MyFirstProgram.java
@@ -5,27 +5,29 @@ public class MyFirstProgram {
     System.out.println("Hello, world!");
     //hello("world");
 
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x = 2;
-    p1.y = 5;
-    p2.x = 10;
-    p2.y = 8;
-    double res = distance (p1, p2);
+    //Point p1 = new Point();
+    //Point p2 = new Point();
+    Point p = new Point ();
+    p.x1 = 2;
+    p.y1 = 5;
+    p.x2 = 10;
+    p.y2 = 8;
 
+    p.distance();
 
+  //  double res = distance (p1, p2);
     //   System.out.println("rasstoyanie mezhdu dvumya tochkami " + " = " + res);
   }
 
-  public static void hello(String somebody) {
+  /*public static void hello(String somebody) {
     System.out.println("Hello, " + somebody +  "!");
-  }
+  }*/
 
-  public static double distance (Point p1, Point p2) {
+ /* public static double distance (Point p1, Point p2) {
     double temp = ((p2.x - p1.x)*(p2.x - p1.x))+((p2.y - p1.y)*(p2.y - p1.y));
     double res = Math.sqrt(temp);
     System.out.println("the distance is " + res +  "!");
     return (res);
-  }
+  }*/
 
 }
--- sandbox/src/main/java/il/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/il/stqa/pft/sandbox/Point.java
@@ -4,6 +4,16 @@ package il.stqa.pft.sandbox;
  * Created by Irena on 8/13/2016.
  */
 public class Point {
-  public double x;
-  public double y;
+   double x1;
+   double y1;
+   double x2;
+   double y2;
+
+
+  public  double distance () {
+    double temp = ((x2 - x1)*(x2 - x1))+((y2 - y1)*(y2 - y1));
+    double res = Math.sqrt(temp);
+    System.out.println("the distance is " + res +  "!");
+    return (res);
+  }
 }
