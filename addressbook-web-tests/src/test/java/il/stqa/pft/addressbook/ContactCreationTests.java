98d3cb31e43d8deaabda5383fb2da0af5b637c80
IrinaMos <irena.moskovich@gmail.com>
2016-08-20 20:06:08 +0300

    ContactCreationTests
    The test contains:
    Create new contact by recording and change record data to methods

---
+++ addressbook-web-tests/src/test/java/il/stqa/pft/addressbook/ContactCreationTests.java
@@ -0,0 +1,81 @@
+package il.stqa.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ContactCreationTests {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.cssSelector("html")).click();
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    @Test
+    public void testContactCreation() {
+        initContact();
+        fillContactData(new ContactData("Irina", "0541112233", "Random", "Israel", "Havkina"));
+        submitContactData();
+    }
+
+    private void submitContactData() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void fillContactData(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getMobileNum());
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(contactData.getCompanyName());
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(contactData.getAdress());
+    }
+
+    private void initContact() {
+        wd.findElement(By.name("searchform")).click();
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
---
+++ addressbook-web-tests/src/test/java/il/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package il.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstName;
+  private final String mobileNum;
+  private final String companyName;
+  private final String address;
+  private final String lastName;
+
+  public ContactData(String firstName, String mobileNum, String companyName, String address, String lastName) {
+    this.firstName = firstName;
+    this.mobileNum = mobileNum;
+    this.companyName = companyName;
+    this.address = address;
+    this.lastName = lastName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMobileNum() {
+    return mobileNum;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public String getAdress() {
+    return address;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+}
