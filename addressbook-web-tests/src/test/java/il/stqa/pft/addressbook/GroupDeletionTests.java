5ac923b8cf41d8d4d42a610af61cb1727809ccc2
IrinaMos <irena.moskovich@gmail.com>
2016-08-20 20:59:51 +0300

    GroupDeletionTest with TestBase

---
+++ addressbook-web-tests/src/test/java/il/stqa/pft/addressbook/GroupDeletionTests.java
@@ -0,0 +1,16 @@
+package il.stqa.pft.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+    @Test
+    public void testGroupDeletion() {
+        GotoGroupPage();
+        selectGroups();
+        deleteGroups();
+        returnToGroupPage();
+    }
+
+
+}
