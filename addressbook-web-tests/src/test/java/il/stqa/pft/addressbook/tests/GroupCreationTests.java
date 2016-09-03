dbeb492f3459f1dfa0fabde87e7130683c1ddef3
IrinaMos <irena.moskovich@gmail.com>
2016-09-03 22:50:38 +0300

    Лямбда-выражения (анонимные функции)
    Creation groups test

--- addressbook-web-tests/src/test/java/il/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/il/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,7 +13,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().GotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
@@ -23,13 +23,14 @@ public class GroupCreationTests extends TestBase {
     // app.getGroupHelper().returnToGroupPage();
 
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+ //   int max = 0;
+  //  for (GroupData g : after) {
+  //    if (g.getId() > max) {
+  //      max = g.getId();
+   //   }
+   // }
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
