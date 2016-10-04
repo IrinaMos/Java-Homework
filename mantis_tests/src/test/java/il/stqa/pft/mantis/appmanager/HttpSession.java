71b4478b85743e54f49f6a9c31d9a9288c3741fd
IrinaMos <irena.moskovich@gmail.com>
2016-10-04 11:47:30 +0300

    Прямое взаимодействие с сервером по протоколу HTTP

--- mantis_tests/src/test/java/il/stqa/pft/mantis/appmanager/HttpSession.java
+++ mantis_tests/src/test/java/il/stqa/pft/mantis/appmanager/HttpSession.java
@@ -28,7 +28,7 @@ public class HttpSession {
   }
 
   public boolean login(String username, String password) throws IOException {
-    HttpPost post = new HttpPost(app.getProperty("web.base  Url") + "/login.php");
+    HttpPost post = new HttpPost(app.getProperty("web.baseUrl") + "/login.php");
     List<NameValuePair> params = new ArrayList<>();
     params.add(new BasicNameValuePair("username", username));
     params.add(new BasicNameValuePair("password", password));
@@ -37,7 +37,7 @@ public class HttpSession {
     post.setEntity(new UrlEncodedFormEntity(params));
     CloseableHttpResponse response = httpclient.execute(post);
     String body = geTextFrom(response);
-    return body.contains(String.format("<span class=\"italic\">%s</span", username));
+    return body.contains(String.format("<span id=\"logged-in-user\">%s</span", username));
   }
 
   private String geTextFrom(CloseableHttpResponse response) throws IOException {
@@ -52,6 +52,6 @@ public class HttpSession {
     HttpGet get = new HttpGet(app.getProperty("web.baseUrl") + "/index.php");
     CloseableHttpResponse response = httpclient.execute(get);
     String body = geTextFrom(response);
-    return body.contains(String.format("<span class=\"italic\">%s</span", username));
+    return body.contains(String.format("<span id=\"logged-in-user\">%s</span", username));
   }
 }
